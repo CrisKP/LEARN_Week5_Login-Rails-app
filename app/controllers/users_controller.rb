cc1016a4d9140c026a6534c47fa9a64caee779ec
learn <learn@learn06.local>
2017-11-03 16:01:34 -0700

    created registration information page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,11 +1,15 @@
 class UsersController < ApplicationController
 
+
   def create
+    @current_users = ["Cris","Greg","VRon","Frank"]
     params.permit(:username, :password, :name, :street, :city, :state, :postal, :country, :email)
 
     if params.has_key?(:username) && params.has_key?(:password)
 
-      if params[:username] != nil && params[:password] != nil
+      if @current_users.include?(params[:username])
+        flash[:notice] = "username is already in use"
+      elsif params[:username] != nil && params[:password] != nil
         @user = User.new(params[:username], params[:password], params[:name], params[:street], params[:city], params[:state], params[:postal], params[:country], params[:email])
         session[:user] = @user.to_yaml
         redirect_to '/users/confirmation'
@@ -13,15 +17,21 @@ class UsersController < ApplicationController
     end
   end
 
+
   def confirmation
     @user = YAML.load(session[:user])
     if params.has_key?(:submit)
       if params[:username] == @user.show_username && params[:password] == @user.show_password
       redirect_to '/users/registration'
       else
-          flash[:notice] = "username and password not found"
+          flash[:alert] = "username and password not found"
           # render 'confirmation.html.erb'
       end
     end
+  end
+  def registration
+    @user = YAML.load(session[:user])
+
+
   end
 end
